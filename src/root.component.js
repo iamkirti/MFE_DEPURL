999a2cb32165541559745a6bebd7a9fc3e37a48b
ksahai <ksahai@opentext.com>
2023-07-25 16:16:10 +0530

    fourth commit

---
+++ src/root.component.js
@@ -0,0 +1,10 @@
+export default function Root(props) {
+  return (
+    <section>
+      {" "}
+      {props.name} is mounted!
+      <br />
+      <a href="/bhavya">Kirtis React</a>
+    </section>
+  );
+}
